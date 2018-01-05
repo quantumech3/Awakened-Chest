db3262d2399cc18308eace0482d1168cca79ee57
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2018-01-05 15:16:28 -0500

    Refactored code to my style

--- src/main/java/com/awakenedChest/awakenedchest/AwakenedChest.java
+++ src/main/java/com/awakenedChest/awakenedchest/AwakenedChest.java
@@ -3,16 +3,26 @@ package com.awakenedChest.awakenedchest;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.lwjgl.Sys;
 
 @Mod(modid = AwakenedChest.MODID, version = AwakenedChest.VERSION,useMetadata = true)
 public class AwakenedChest
 {
     public static final String MODID = "awakenedchest";
     public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
 
-    }
+    @EventHandler
+    public void PreInit(FMLPreInitializationEvent event){
+
+
+
+    }//void PreInit: Runs before the game finishes launching
+
+    @EventHandler
+    public void Init(FMLInitializationEvent event){
+
+        
+
+    }//void init: Runs at the beginning of the game launch
 }

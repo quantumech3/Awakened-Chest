e7161405f68d4d61de087927df7dcff187bd8314
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2018-01-05 15:07:16 -0500

    Changed modid and everything to be for the Awakened Chest mod

--- src/main/java/com/awakenedChest/awakenedchest/AwakenedChest.java
+++ src/main/java/com/awakenedChest/awakenedchest/AwakenedChest.java
@@ -1,14 +1,13 @@
-package com.example.examplemod;
+package com.awakenedChest.awakenedchest;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = AwakenedChest.MODID, version = AwakenedChest.VERSION,useMetadata = true)
+public class AwakenedChest
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "awakenedchest";
     public static final String VERSION = "1.0";
     
     @EventHandler
